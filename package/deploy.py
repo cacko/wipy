dc593d2393345b1cd0814aefeda6f60c99cacc5b
cacko <alex@cacko.net>
2021-09-26 21:29:33 +0100

    foo

---
+++ package/deploy.py
@@ -0,0 +1,16 @@
+import gitlab
+
+gl = gitlab.Gitlab(url="https://gitlab.com", private_token='qMm4P4GYds_ah6h-NBbt')
+
+gl.auth()
+
+
+project_name_with_namespace = "cacko/streamy"
+project = gl.projects.get(project_name_with_namespace)
+
+package = project.generic_packages.upload(
+    package_name="wipy",
+    package_version="v1.0.0",
+    file_name="wipy.dmg",
+    path="./wipy.dmg"
+)
